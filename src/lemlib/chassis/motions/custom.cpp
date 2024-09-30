a6a168dd045ce10a436aafd80d304da7ac69337c
ilovecrayons <huangfeiyang2008@gmail.com>
2024-09-30 08:55:43 -0700

    moveFor v1

--- src/lemlib/chassis/motions/custom.cpp
+++ src/lemlib/chassis/motions/custom.cpp
@@ -26,6 +26,7 @@ void lemlib::Chassis::moveFor(float distance, int timeout, MoveForParams params,
 
     // initialize vars used between iterations
     Pose lastPose = getPose();
+    float currentAngle = getPose().theta;
     distTraveled = 0;
     Timer timer(timeout);
     bool close = false;
@@ -53,17 +54,74 @@ void lemlib::Chassis::moveFor(float distance, int timeout, MoveForParams params,
         }
 
         // motion chaining
-        const float lateralMovement = pose.x - lastPose.x;
-        const bool side = lateralMovement > 0;
-        if (prevSide == std::nullopt) prevSide = side;
-        const bool sameSide = side == prevSide;
-        // exit if close
-        if (!sameSide && params.minSpeed != 0) break;
-        prevSide = side;
+        // calculate angle difference between current pose and target theta angle
+        const float currentAngleDiff = angleError(currentAngle, pose.theta, false);
+
+        // calculate angle difference between previous pose and target theta angle
+        const float previousAngleDiff = angleError(currentAngle, lastPose.theta, false);
+
+        // exit if signs of angle differences are different and params.minSpeed is non-zero
+        if (sgn(currentAngleDiff) != sgn(previousAngleDiff) && params.minSpeed != 0) break;
 
         // calculate error
         const float adjustedRobotTheta = params.forwards ? pose.theta : pose.theta + M_PI;
-        const float angularError = angleError(
-        )
+        const float angularError = angleError(adjustedRobotTheta, currentAngle);
+        
+        lateralSmallExit.update(distTarget);
+        lateralLargeExit.update(distTarget);
+
+        // get output for PIDs
+        float lateralOut = lateralPID.update(distTarget);
+        float angularOut = angularPID.update(radToDeg(angularError));
+        if (close) angularOut = 0;
+
+        // apply restrictions on angular speed
+        angularOut = std::clamp(angularOut, -params.maxSpeed, params.maxSpeed);
+        angularOut = slew(angularOut, prevAngularOut, angularSettings.slew);
+
+        // apply restrictions on lateral speed
+        lateralOut = std::clamp(lateralOut, -params.maxSpeed, params.maxSpeed);
+
+        //constrain lateral output by max accel, but not for deceleration
+        if (!close) lateralOut = slew(lateralOut, prevLateralOut, lateralSettings.slew);
+
+        //prevent moving in the wrong direction
+        if (params.forwards && !close) lateralOut = std::fmax(lateralOut, 0);
+        else if (!params.forwards && !close) lateralOut = std::fmin(lateralOut, 0);
+
+        // constrain lateral output by the minimum speed
+        if (params.forwards && lateralOut < fabs(params.minSpeed) && lateralOut > 0) lateralOut = fabs(params.minSpeed);
+        if (!params.forwards && -lateralOut < fabs(params.minSpeed) && lateralOut < 0)
+            lateralOut = -fabs(params.minSpeed);
+
+        // update previous output
+        prevAngularOut = angularOut;
+        prevLateralOut = lateralOut;
+
+        infoSink()->debug("Angular Out: {}, Lateral Out: {}", angularOut, lateralOut);
+
+        // ratio the speeds to respect the max speed
+        float leftPower = lateralOut + angularOut;
+        float rightPower = lateralOut - angularOut;
+        const float ratio = std::max(std::fabs(leftPower), std::fabs(rightPower)) / params.maxSpeed;
+        if (ratio > 1) {
+            leftPower /= ratio;
+            rightPower /= ratio;
+        }
+
+        // move the drivetrain
+        drivetrain.leftMotors->move(leftPower);
+        drivetrain.rightMotors->move(rightPower);
+
+        // delay to save resources
+        pros::delay(10);
+    }
+
+    // stop the drivetrain
+    drivetrain.leftMotors->move(0);
+    drivetrain.rightMotors->move(0);
+    // set distTraveled to -1 to indicate that the function has finished
+    distTraveled = -1;
+    this->endMotion();
 
 }
