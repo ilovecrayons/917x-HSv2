ff8935effad4cc0936a135614c1ec44b3b9b7ed3
ilovecrayons <huangfeiyang2008@gmail.com>
2025-02-09 10:18:12 -0700

    WORKING= RED, blue is close to crossing

--- src/main.cpp
+++ src/main.cpp
@@ -283,10 +283,10 @@ void mogoRushRed() {
     chassis.moveToPose(-20, -17, 180, 2000, {.forwards = false, .maxSpeed = 70}, true);
     chassis.waitUntil(33);
     clamp.set_value(true);
-    chassis.moveToPoint(-10, -56, 2000, {.maxSpeed = 70}, false); // was 4,-61
+    chassis.moveToPoint(-10, -57, 2000, {.maxSpeed = 70}, false); // was 4,-61
     chassis.turnToHeading(+90 + 39, 2000, {}, false); // was -90-42
-    chassis.moveFor(7, 2000, {}, false);
-    chassis.turnToHeading(90 + 48, 1000, {}, false);
+    chassis.moveFor(7.2, 2000, {}, false); //7
+    chassis.turnToHeading(90 + 45, 1000, {}, false); //was 48
     intake.set(Intake::IntakeState::STOPPED);
     arm.scoreWallstake();
     intake.set(Intake::IntakeState::OUTTAKE, 90);
@@ -297,11 +297,14 @@ void mogoRushRed() {
     chassis.turnToPoint(-58, -26, 2000, {}, true);
     intake.set(Intake::IntakeState::INTAKING);
     chassis.moveToPose(-59,-25,-90,2000,{},true);
-    pros::delay(1000);
-    clamp.set_value(true);
+    chassis.moveFor(15, 2000, {.forwards = false},false);
+    pros::delay(500);
+    clamp.set_value(false);
+    chassis.moveFor(10, 2000, {},false);
+    chassis.turnToHeading(180, 2000, {}, false);
 }
 
-void mogoRushBlue() {
+void  mogoRushBlue() {
     chassis.setPose(54, -36, -90);
     intake.set(Intake::IntakeState::INTAKING, 127);
     chassis.moveToPose(12, -50, -105, 2000, {.maxSpeed = 100}, true);
@@ -315,13 +318,13 @@ void mogoRushBlue() {
     chassis.turnToHeading(90, 2000, {.direction = AngularDirection::CCW_COUNTERCLOCKWISE, .maxSpeed = 90}, false);
     hook.set_value(false);
     // chassis.turnToPoint(24,-15,2000,{.forwards = false},false);
-    chassis.moveToPoint(12,-47,2000,{.forwards = false,.maxSpeed = 90,.minSpeed = 40,.earlyExitRange = 10},false);
+    chassis.moveToPoint(15,-47,2000,{.forwards = false,.maxSpeed = 90,.minSpeed = 40,.earlyExitRange = 10},false);
     chassis.moveToPose(23, -19, -180, 2000, {.forwards = false, .maxSpeed = 70}, true); // was 23,-20 // was 21, -14.5
     chassis.waitUntil(32);
     clamp.set_value(true);
     chassis.moveToPoint(7, -61, 3000, {.maxSpeed = 80}, false); // was 4,-61
     chassis.turnToHeading(-90 - 50, 2000, {}, false); // was -90-48
-    chassis.moveFor(3.5, 2000, {}, false);
+    chassis.moveFor(3, 2000, {}, false);
     chassis.turnToHeading(-90 - 47, 500, {}, false);
     intake.set(Intake::IntakeState::STOPPED);
     arm.scoreWallstake();
@@ -676,10 +679,10 @@ void autonomous() {
     // chassis.moveToPoint(0,0, 10000, {.maxSpeed = 80});
     // chassis.waitUntilDone();
     //halfAwpRed();
-    mogoRushRed();
-    // mogoRushBlue();
+    //mogoRushRed();
+    mogoRushBlue();
     // awpRed();
-    // awpBlue();
+    //  awpBlue();
     // elimRedTopSide();
     //elimBlueTopSide();
 
